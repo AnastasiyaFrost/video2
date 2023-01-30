92868caee21051b7d95339ab709a434e8ef3c701
user <grebneva.nastena@yandex.ru>
2023-01-30 18:37:38 +0300

    Commit1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book book1 = new Book("TITLE1", "author1");
+        Book book2 = new Book("TITLE2", "author2");
+        Book book3 = new Book("TITLE3", "author3");
+        Book[] books = {book1, book2, book3};
+        for (Book book : books) {
+            System.out.println(book);
+        }
     }
 }
